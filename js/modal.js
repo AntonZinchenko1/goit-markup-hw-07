85dd14b2967e754c31aa3a466ac0893f124f6858
AntonZinchenko1 <zinantigr@gmail.com>
2023-05-01 07:17:32 +0300

    Добавлены модальное описание

--- js/modal.js
+++ js/modal.js
@@ -8,7 +8,18 @@
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
+  // **  Закрытие модального окна по бекдропу\\\\\\\
+
+  refs.modal.addEventListener("click", removeModal);
+  function removeModal(e) {
+    if (e.farget === e.currentTarget) {
+      refs.modal.classList.add("is-hidden");
+      document.body.classList.remove("no-scroll");
+    }
+  }
+
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
   }
 })();
